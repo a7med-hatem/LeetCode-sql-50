db7ac726df84db65e71649312f28ae9c096c5f5f
Ahmed <ahmedhatem12676@gmail.com>
2026-06-11 00:25:02 +0300

    Advanced-String-Functions-Regex-Clause DONE

---
+++ LeetCode_SQL_50/Advanced-String-Functions-Regex-Clause/1484-Group-Sold-Products-By-The-Date.sql
@@ -0,0 +1,13 @@
+WITH UniqueActivities AS (
+    SELECT DISTINCT
+        sell_date,
+        product
+    FROM Activities
+)
+SELECT
+    sell_date,
+    COUNT(product) AS num_sold,
+    STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM UniqueActivities
+GROUP BY sell_date
+ORDER BY sell_date
